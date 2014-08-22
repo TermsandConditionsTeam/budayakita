2fd63ed5ea1bd7f98fb58e65cc524696a295eab0
termsconditions <rifqithomi@gmail.com>
2014-08-22 14:34:18 +0700

    Update index.php
    add session for visitors

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+  session_start();
+  $_SESSION['username'] = "tomtom";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,9 +24,35 @@
         <div class="logo">
           <img src="assets/images/a.png" width="300" height="86" >
         </div>
-        <div class="login">
-          <div class="user">Welcome!</div>
-          <a href="">Masuk</a> | <a href="">Daftar</a>
+        <div class="login">          
+          <div class="user">Welcome!, 
+            <?php
+              if(!isset($_SESSION['username']))
+              {
+                echo 'Visitor
+                      </div>
+                      <a href="">Masuk</a> | <a href="">Daftar</a>   
+                ';
+              }
+              else
+                echo $_SESSION['username'].'
+                      </div>
+                      <ul style="list-style:none;">
+                        <li class="dropdown">
+                          <a style="height:30px; margin-right:10px;margin-top:-10px" href="#" data-toggle="dropdown">Profile <b class="caret"></b></a>
+                          <ul class="dropdown-menu">
+                            <li><a href="#">Edit Profile</a></li>
+                            <li><a href="#">Lencanaku</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Nav header</li>
+                            <li><a href="#">Separated link</a></li>
+                            <li><a href="#">One more separated link</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                  ';
+            ?>
         </div>
       </div>
     </header>
