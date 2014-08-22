38d870919f4c345f9739a9b15f49f9db3ed42c85
termsconditions <rifqithomi@gmail.com>
2014-08-22 13:24:55 +0700

    new upload
    New Upload all files.

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--link rel="shortcut icon" href="../../assets/ico/favicon.ico"-->
+    <title>Budaya Kita</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    
+
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+        <div class="logo">
+          <img src="assets/images/a.png" width="300" height="86" >
+        </div>
+        <div class="login">
+          <div class="user">Welcome!</div>
+          <a href="">Masuk</a> | <a href="">Daftar</a>
+        </div>
+      </div>
+    </header>
+    <nav>
+      <div class="container">
+           <ul style="height:30px; margin-top:5px;margin-left:-20px;list-style: none;" class="navbar-nav">
+              <li class="dropdown">
+                <a style="height:30px; margin-right:10px;margin-top:-10px" href="#" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a style="height:30px;" href="#" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+           </ul>
+      </div>
+    </nav>
+
+    <div class="content">
+      <div class="container">
+        <div class="isi">
+          <img src="a" width="970px"; height="400px";>
+        </div>
+      </div>
+    </div>
+
+    <div class="fitur">
+      <div class="container">
+        <div class="row">
+           <div class="col-xs-4">
+            <div class="partner-item">
+              <img src="assets/images/partners/partner1.png" alt="">
+            </div> <!-- /.partner-item -->
+           </div>
+           <div class="col-xs-4">
+            <div class="partner-item">
+              <img src="assets/images/partners/partner2.png" alt="">
+            </div> <!-- /.partner-item -->
+           </div>
+           <div class="col-xs-4">
+            <div class="partner-item">
+              <img src="assets/images/partners/partner3.png" alt="">
+            </div> <!-- /.partner-item -->
+           </div>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="container">
+        a
+      </div>
+    </footer>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
